3516bab5778082870add3835305c8be289341d7c
Johnson <johnsonta87@gmail.com>
2023-04-03 10:11:37 -0400

    revamp with tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnsonta.netlify.app",
   integrations: [image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), react()]
+  }), mdx(), sitemap(), react(), tailwind()]
 });
